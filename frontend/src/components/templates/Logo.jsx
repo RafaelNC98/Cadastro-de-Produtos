ebc6fe19f4e9c5cda3007c59eea421bba3e9f9d1
RafaelNC98 <rafaelnadalinicarvalho@gmail.com>
2021-04-01 15:29:14 -0300

    adicionando estilos css e logo

--- frontend/src/components/templates/Logo.jsx
+++ frontend/src/components/templates/Logo.jsx
@@ -1,7 +1,10 @@
 import './Logo.css'
+import logo from '../../assets/imgs/cart.png'
 import React from 'react'
 
 export default props =>
     <aside className="logo">
-        Logo
+        <a href="" className="logo">
+            <img src={logo} alt=""/>
+        </a>
     </aside>
