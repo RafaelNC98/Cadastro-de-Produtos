c89dc8c4f51c22117c4cb99b5a5168fcd3d727f1
RafaelNC98 <rafaelnadalinicarvalho@gmail.com>
2021-04-01 21:54:52 -0300

    finalizado o css

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Main from '../templates/Main'
+
+export default props =>  
+    <Main icon="home" title="Início"
+        subtitle="Seja Bem-Vindo...">
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">
+            Fazendo um teste...
+        </p>
+
+    </Main>
